03e17c19e959be39884e4aeb855a28bf6783785d
Abisai Santos <abisaiweb7@gmail.com>
2021-01-11 23:37:45 -0300

    Home ok

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
-var slideIndex = 0;
-showSlides();
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  //Fazendo aparecer os itens
+  const navLinks = document.querySelectorAll('.nav-links li');
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  //Fazendo o clique do menu funcionar
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    //Animações links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navlinkFade 0.5s ease forwards ${index/7+1.5}s`
+
+      }
+    });
+    //Transformando em X
+    burger.classList.toggle('toggle');
+
+
+  });
+
+
 }
+
+navSlide();
---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+	new SimpleSlide({
+		slide: "quote", // nome do atributo data-slide="principal"
+		time: 5000 // tempo de transição dos slides
+	});
+
+	new SimpleSlide({
+		slide: "index",
+		time: 5000,
+		nav: true
+	});
+
+
+
+console.log('teste')
