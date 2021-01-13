e64f8faf12bbdae6cfa68a4fe110cc51669034d2
Abisai Santos <abisaiweb7@gmail.com>
2021-01-13 17:15:17 -0300

    Slides criados

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+	if(window.SimpleSlide) {
 
-
-	new SimpleSlide({
-		slide: "quote", // nome do atributo data-slide="principal"
-		time: 5000 // tempo de transição dos slides
-	});
-
-	new SimpleSlide({
-		slide: "index",
-		time: 5000,
-		nav: true
-	});
+		new SimpleSlide({
+			slide: "quote", // nome do atributo data-slide="principal"
+			time: 5000 // tempo de transição dos slides
+		});
+	
+		new SimpleSlide({
+			slide: "slider-principal",
+			time: 5000,
+			nav: true
+		});
+	}
 
 
 
